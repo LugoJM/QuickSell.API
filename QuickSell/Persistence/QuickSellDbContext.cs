6454d4e33ab94a55b935f012725712a0673933ae
JM Lugo <lugoc.jose@outlook.com>
2024-05-09 20:22:50 -0700

    Declared table collections and SaleItem relationship

--- QuickSell/Persistence/QuickSellDbContext.cs
+++ QuickSell/Persistence/QuickSellDbContext.cs
@@ -1,8 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using QuickSell.Domain;
 
 namespace QuickSell.Persistence;
 
 public class QuickSellDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Sale> Sales { get; set; }
+    public DbSet<SaleItem> SaleItems { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SaleItem>()
+            .HasOne(si => si.Product)
+            .WithMany(p => p.SaleItems)
+            .HasForeignKey(si => si.ProductId);
+    }
 }
